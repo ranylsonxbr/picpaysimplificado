ebc49a5289c81286548ee285b31862e9e76a0ab0
ranylsonxbr <ranylson1212@gmail.com>
2023-12-31 16:41:00 -0300

    implementa dominio

---
+++ src/main/java/com/picpaysimplificado/user/domain/User.java
@@ -0,0 +1,28 @@
+package com.picpaysimplificado.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@EqualsAndHashCode(of="id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+
+}
